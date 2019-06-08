72c5041d201cbd4243d00d7d7b3c0be03a1360c2
petrosdeb <petrosd@student.chalmers.se>
2019-06-08 17:33:20 +0800

    Processing

--- data processing/testprocess.py
+++ data processing/testprocess.py
@@ -1,8 +1,8 @@
 NUMBER_OF_LINES = 22284  # number of lines/genes in microarray_original.txt
 # GPL96-15653.txt also has 22284 lines (plus 16 more for explanation)
 # microarray_original.txt has 5897 columns, the number of observations
-file_dir = "D:\\Pritom Lab\Sjtu\\2nd Semester\\Bio Info\\Gene_Chip_Data\\Gene_Chip_Data"
-
+file_dir = 'C:/Users/Petros Debesay/Downloads/Gene_Chip_Data'
+import csv
 
 def read_only_lines(f, start, finish):
     for ii, line in enumerate(f):
@@ -12,11 +12,44 @@ def read_only_lines(f, start, finish):
             return
 
 
+def mean_around_zero(string_list):
+    total = 0
+    zero_mean_list = []
+
+    for i in range(1, len(string_list)):
+        current = float(string_list[i])
+        total += current
+        zero_mean_list.append(current)
+
+    mean = total / (len(string_list) - 1)
+
+    for x in range(0, len(zero_mean_list)):
+        zero_mean_list[x] -= mean
+
+    return zero_mean_list
+
+
+def normalize(lst, max_val):
+    for i in range(0, len(lst)):
+        lst[i] /= max_val
+
+    return lst
+
 # 5,897 columns in each line
 # lines start with #ID (Affymetrix Probe Set ID),
 # then succeeded by numbers (breakdown of appearance of each probe/RMA signal?)
-line_start = 0
-for line in read_only_lines(open(file_dir + '\\microarray_original.txt'), line_start, line_start + 5):
-    print(line.split()[0])
+line_start = 1
+line_end = 22284
+for line in read_only_lines(open('C:/Users/Petros Debesay/Downloads/Gene_Chip_Data/microarray.original.txt'), line_start, line_end):
+    mean_zero = mean_around_zero(line.split())
+    max_val = max(mean_zero, key=abs)
+
+    mean_zero = normalize(mean_zero, abs(max_val))
+
+    print(mean_zero)
+
+    with open('C:/Users/Petros Debesay/PycharmProjects/BioInfoML/Data/processed.csv', 'a') as file:
+        writer = csv.writer(file)
+        writer.writerow(mean_zero)
 # for word in line.split():
 #	print(word)
