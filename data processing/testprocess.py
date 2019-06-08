b8057171fd2d395cd4764eec9002165d20805e19
pritom <14302016002@fudan.edu.cn>
2019-06-08 15:20:33 +0800

    first commit
    Signed-off-by: pritom <14302016002@fudan.edu.cn>

--- data processing/testprocess.py
+++ data processing/testprocess.py
@@ -1,7 +1,7 @@
 NUMBER_OF_LINES = 22284  # number of lines/genes in microarray_original.txt
 # GPL96-15653.txt also has 22284 lines (plus 16 more for explanation)
 # microarray_original.txt has 5897 columns, the number of observations
-file_dir = "D:\\Pritom Lab\Sjtu\\2nd Semester\\Bio Info\\Gene_Chip_Data\\Gene_Chip_Data"
+file_dir = "D:\\Pritom Lab\Sjtu\\2nd Semester\\Bio Info\\Gene_Chip_Data\\Gene_Chip_Data\\"
 
 
 def read_only_lines(f, start, finish):
@@ -16,7 +16,7 @@ def read_only_lines(f, start, finish):
 # lines start with #ID (Affymetrix Probe Set ID),
 # then succeeded by numbers (breakdown of appearance of each probe/RMA signal?)
 line_start = 0
-for line in read_only_lines(open(file_dir + '\\microarray_original.txt'), line_start, line_start + 5):
-    print(line.split()[0])
+for line in read_only_lines(open(file_dir + 'microarray.original.txt'), line_start, line_start + 6):
+    print(line.split("	"))
 # for word in line.split():
 #	print(word)
