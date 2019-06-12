e4fa06e497836dde332c3f677c4c25173664257d
petrosdeb <petrosd@student.chalmers.se>
2019-06-12 12:55:36 +0800

    Normalize and dump

--- data processing/testprocess.py
+++ data processing/testprocess.py
@@ -1,10 +1,14 @@
+import csv
+from sklearn.preprocessing import StandardScaler
+import numpy as np
+from sklearn.decomposition import PCA
+
 NUMBER_OF_LINES = 22284  # number of lines/genes in microarray_original.txt
 # GPL96-15653.txt also has 22284 lines (plus 16 more for explanation)
 # microarray_original.txt has 5897 columns, the number of observations
 
-import csv
 
-file_dir = "D:\\Pritom Lab\Sjtu\\2nd Semester\\Bio Info\\Gene_Chip_Data\\Gene_Chip_Data\\"
+file_dir = "C:/Users/Petros Debesay/Downloads/Gene_Chip_Data/"
 output_file = 'C:/Users/Petros Debesay/PycharmProjects/BioInfoML/Data/processed.csv'
 
 
@@ -39,22 +43,36 @@ def normalize(lst, max_val):
 
     return lst
 
+
+def publish(publish_list):
+    publish_list = StandardScaler().fit_transform(publish_list)
+    with open(output_file, 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(publish_list)
 # 5,897 columns in each line
 # lines start with #ID (Affymetrix Probe Set ID),
 # then succeeded by numbers (breakdown of appearance of each probe/RMA signal?)
 line_start = 1
-line_end = 22284
+line_end = NUMBER_OF_LINES
+x = []
+counter = 0
 for line in read_only_lines(open(file_dir + 'microarray.original.txt'), line_start, line_end):
-    mean_zero = mean_around_zero(line.split())
-    max_val = max(mean_zero, key=abs)
 
-    mean_zero = normalize(mean_zero, abs(max_val))
+    word_list = line.split()
+    float_list = []
+    for i in range(1, len(word_list)):
+        current = float(word_list[i])
+        float_list.append(current)
 
-    print(mean_zero)
+    x.append(float_list)
 
-    with open(output_file, 'a') as file:
-        writer = csv.writer(file)
-        writer.writerow(mean_zero)
+x = np.array(x).transpose()
+x = StandardScaler().fit_transform(x)
+np.savetxt("processed.csv", x, delimiter=",")
+#with open(output_file, 'a') as file:
+
+    #writer = csv.writer(file)
+    #writer.writerow(x)
 
 # for word in line.split():
 #	print(word)
