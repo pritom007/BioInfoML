0017fc433de722dcabaed48237f73efbf5e9e9b4
petrosdeb <petrosd@student.chalmers.se>
2019-06-14 14:17:31 +0800

    Improved PCA

--- PCA/pca_test.py
+++ PCA/pca_test.py
@@ -3,12 +3,19 @@ from sklearn.decomposition import PCA
 
 file_dir = 'C:/Users/Petros Debesay/PycharmProjects/BioInfoML/data processing/processed.csv'
 
+number_components = 100
+
 df = pd.read_csv(file_dir)
 
-pca = PCA(n_components=2)
+pca = PCA(n_components=number_components)
+
+components = []
+
+for x in range(0, number_components):
+    components.append("Component" + str(x))
 
 principal_components = pca.fit_transform(df)
 
-principal_df = pd.DataFrame(data=principal_components, columns=["test1", "test2"])
+principal_df = pd.DataFrame(data=principal_components, columns=components)
 
 print(principal_df)
